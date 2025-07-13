6d894fd97eedc42c0cf269ee47f924981f88b33e
pavelGavrilenko <99758732+pavelGavrilenko@users.noreply.github.com>
2025-07-13 22:04:40 +0300

    Углубление в Playwright.Практика по углубленной работе с Playwright
    Правки по оценке задания

--- playwright_registration_button.py
+++ playwright_registration_button.py
@@ -7,7 +7,7 @@ with sync_playwright() as playwright:
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
     reg_button = page.get_by_test_id("registration-page-registration-button")
-    reg_button.is_disabled()
+    expect(reg_button).to_be_disabled()
 
     email = page.get_by_test_id("registration-form-email-input").locator("input")
     email.fill('user.name@gmail.com')
@@ -19,4 +19,4 @@ with sync_playwright() as playwright:
     password.fill('password')
 
 
-    reg_button.is_enabled()
+    expect(reg_button).to_be_enabled()
