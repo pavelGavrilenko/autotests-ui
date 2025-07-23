982f428cbedbb4b73187f1dda930a552d2ec96be
pavelGavrilenko <99758732+pavelGavrilenko@users.noreply.github.com>
2025-07-23 22:28:48 +0300

    Знакомство с PageObject.Практика работы с Page Object

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,24 +1,24 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect
 
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration(chromium_page):
-    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+@pytest.mark.parametrize(
+    "email, password, username",
+    [
+        ("user.name@gmail.com", "password", "username")
+    ]
+)
+def test_successful_registration(registration_page,
+                                   dashboard_page,
+                                   email: str,
+                                   username: str,
+                                   password: str):
+    registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    registration_page.fill_registration_form(email=email,
+                                             password=password,
+                                             username=username
+                                             )
+    registration_page.click_reg_button()
+    dashboard_page.check_visible_dashboard_tittle()
 
-    email = chromium_page.get_by_test_id("registration-form-email-input").locator("input")
-    email.fill('user.name@gmail.com')
-
-    username = chromium_page.get_by_test_id("registration-form-username-input").locator("input")
-    username.fill('username')
-
-    password = chromium_page.get_by_test_id("registration-form-password-input").locator("input")
-    password.fill('password')
-
-    reg_button = chromium_page.get_by_test_id("registration-page-registration-button")
-    reg_button.click()
-
-    dashbord_tittle = chromium_page.get_by_test_id("dashboard-toolbar-title-text")
-    expect(dashbord_tittle).to_be_visible()
-    expect(dashbord_tittle).to_have_text('Dashboard')
