19d44b065884987ab3a0e814077c6b5f80ebe7de
pavelGavrilenko <99758732+pavelGavrilenko@users.noreply.github.com>
2025-07-26 11:25:26 +0300

    Знакомство с PageComponent.Практика работы с Page Component

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -7,14 +7,15 @@ from pages.create_course_page import CreateCoursePage
 
 @pytest.mark.regression
 @pytest.mark.courses
-def test_empty_courses_list(chromium_page_with_state):
+def test_empty_courses_list(chromium_page_with_state,
+                              courses_list_page:CoursesListPage
+                              ):
     chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-
-    course_in_navbar = chromium_page_with_state.get_by_test_id("courses-drawer-list-item-button")
-    expect(course_in_navbar.locator("span")).to_have_text("Courses")
-
-    text_nothing_in_list = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
-    expect(text_nothing_in_list).to_have_text("Results from the load test pipeline will be displayed here")
+    courses_list_page.navbar.check_visible("username")
+    courses_list_page.sidebar.check_visible()
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_empty_view()
 
 
 @pytest.mark.regression
