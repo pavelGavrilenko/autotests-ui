cc5febe088b40b79ee7b88d2ce5be720fe0957fd
pavelGavrilenko <99758732+pavelGavrilenko@users.noreply.github.com>
2025-08-27 10:19:22 +0300

    test aretefact start

--- elements/base_element.py
+++ elements/base_element.py
@@ -1,7 +1,9 @@
 import allure
 from playwright.sync_api import Page, Locator, expect
+from ui_coverage_tool import ActionType, SelectorType
 
 from tools.logger import get_logger
+from elements.ui_coverage import tracker
 
 logger = get_logger("BASE_ELEMENT")
 
@@ -24,6 +26,24 @@ class BaseElement:
             logger.info(step)  # Добавили логирование
             return self.page.get_by_test_id(locator).nth(nth)
 
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        # Возвращает строковое XPath-представление локатора для покрытия.
+        # Важно: Playwright сам не даёт доступ к исходному локатору, поэтому мы формируем его вручную.
+        # Используем XPath, так как он легко поддерживает индексацию ([n+1]).
+        return f"//*[@data-testid='{self.locator.format(**kwargs)}'][{nth + 1}]"
+
+    def track_coverage(self, action_type: ActionType, nth: int = 0, **kwargs):
+        # Трекает действие над элементом, отправляя данные в coverage-трекер.
+        # Передаётся:
+        # - XPath-селектор
+        # - Тип действия (click, visible, text и другие)
+        # - Тип селектора (XPATH)
+        tracker.track_coverage(
+            selector=self.get_raw_locator(nth, **kwargs),
+            action_type=action_type,
+            selector_type=SelectorType.XPATH
+        )
+
     def click(self, nth: int = 0, **kwargs):
         step = f'Clicking {self.type_of} "{self.name}"'
 
@@ -31,6 +51,8 @@ class BaseElement:
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)  # Добавили логирование
             locator.click()
+        # После успешного клика трекаем действие как CLICK
+        self.track_coverage(ActionType.CLICK, nth, **kwargs)
 
     def check_visible(self, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" is visible'
@@ -39,6 +61,8 @@ class BaseElement:
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)  # Добавили логирование
             expect(locator).to_be_visible()
+        # Трекаем видимость как VISIBLE
+        self.track_coverage(ActionType.VISIBLE, nth, **kwargs)
 
     def check_have_text(self, text: str, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" has text "{text}"'
@@ -47,3 +71,5 @@ class BaseElement:
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)  # Добавили логирование
             expect(locator).to_have_text(text)
+        # Трекаем наличие текста как TEXT
+        self.track_coverage(ActionType.TEXT, nth, **kwargs)
